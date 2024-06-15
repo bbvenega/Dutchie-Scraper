d85e56c232ffb6c23cc640856f3ea12b9f6eeeee
bbvenega <bbvenega@asu.edu>
2024-06-14 18:49:50 -0700

    Got it to make individual Sheets

--- DutchieScraper.py
+++ DutchieScraper.py
@@ -39,7 +39,7 @@ warnings.filterwarnings("ignore", category=ResourceWarning)
 
 # The following code is used to create a headless browser using the undetected_chromedriver package
 chromeOptions = uc.ChromeOptions()
-# chromeOptions.headless = True
+chromeOptions.headless = True
 driver = uc.Chrome(use_subprocess=True, options=chromeOptions)
 
 all_products = []
@@ -47,7 +47,7 @@ seen_rows = set()
 
 # The following function is used to login to the Dutchie Backoffice
 def login():
-        driver.get("https://cove.backoffice.dutchie.com/products/inventory?categories=Everyday+14g+Pre-Packed+Flower%2CEveryday+14g+Pre-Packed+Shake+Flower%2CEveryday+28g+Pre-Packed+Flower%2CEveryday+3.5g+Pre-Packed+%28Promo%29+Flower%2CEveryday+3.5g+Pre-Packed+Flower%2CEveryday+7g+Shake+Pre-Packed+Flower%2CLoud+Pax+14g+Pre-Packed+Flower%2CLoud+Pax+28g+Pre-Packed+Flower%2CLoud+Pax+3.5g+Pre-Packed+Flower%2CLoud+Pax+3.5g+Flower+%28MED%29%2CLiquid+Gold+Concentrates%2CLiquid+Gold+CR+Concentrates%2CLiquid+Gold+Infused+Pre-Rolls%2CLiquid+Gold+Kief+Concentrates%2CLiquid+Gold+Kief+Hash+Concentrates%2CLiquid+Gold+Live+Disposable+1g%2CLiquid+Gold+Loaded+1g+Pre-Rolls%2CLiquid+Gold+NANO+Infused+Pre-Rolls%2CCookies+3.5g+Vendor+Pre-Packed+Flower%2CCookies+7g+Vendor+Pre-Packed+Flower%2CCookies+Bulk+Concentrates%2CCookies+1g+LR+Concentrates%2CCookies+1g+Concentrates%2CMAC+Pharms+3.5g+Vendor+Pre-Packed+Flower&sortFields=product.whseProductsDescription&sortDirections=asc&pageSize=1000")
+        driver.get("https://cove.backoffice.dutchie.com/products/inventory?pageSize=1000&categories=Everyday+3.5g+%28Promo%29+Flower%2CEveryday+3.5g+Flower%2CEveryday+14g+Flower%2CEveryday+14g+Shake+Flower%2CEveryday+28g+Flower%2CLoud+Pax+3.5g+Flower%2CLoud+Pax+3.5g+Flower+%28MED%29%2CLoud+Pax+14g+Flower%2CLoud+Pax+28g+Flower%2CCookies+3.5g+Flower%2CCookies+7g+Flower%2CLiquid+Gold+1g+Shatter+Concentrates%2CLiquid+Gold+Concentrates%2CLiquid+Gold+LR+Concentrate&sortFields=product.whseProductsDescription&sortDirections=asc")
         time.sleep(5)
 
         # The following code is used to enter the username and password into the login form
@@ -163,6 +163,22 @@ def fetch_Inventories():
 
 
 
+def mapProducts(products):
+    categorized_products = {}
+    for product in products:
+        substring = product.product_name.split("-")[0].strip()
+        substringCopy = substring
+        if "Liquid Gold" in substring:
+            substring = "Liquid Gold Concentrates"
+            if "Shatter" in substringCopy:
+                substring = "Liquid Gold Shatter"
+        
+        if substring not in categorized_products:
+            categorized_products[substring] = []
+        categorized_products[substring].append(product)
+    return categorized_products
+
+
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SPREADSHEET_ID = spreadSheetID
@@ -174,28 +190,42 @@ service = build('sheets', 'v4', credentials=credentials)
 
 # Prepare the data in the format required by the API
 
-def writeToGoogleSheets(data):
+def writeToGoogleSheets(categoriezed_products):
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
     SPREADSHEET_ID = spreadSheetID
-    RANGE_NAME = 'Sheet1!A2'
     SERVICE_ACCOUNT_FILE = 'service_account.json'
 
     credentials = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
     service = build('sheets', 'v4', credentials=credentials)
 
     # Prepare the data in the format required by the API
-    values = [[product.rowId, product.product_name, product.package_id, product.available, product.inventory_date, product.unit_price, product.batch, product.thc, product.room] for product in data]
+    for category, products in categoriezed_products.items():
+        RANGE_NAME = f"'{category}'!A2"
+        values = [[product.rowId, product.product_name, product.package_id, product.available, product.inventory_date, product.unit_price, product.batch, product.thc, product.room] for product in products]
 
-    body = {
+        body = {
         'values': values
-    }
+        }
 
     # Use the Sheets API to update the sheet
-    result = service.spreadsheets().values().update(
-        spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME,
-        valueInputOption='RAW', body=body).execute()
+        result = service.spreadsheets().values().update(
+            spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME,
+            valueInputOption='RAW', body=body).execute()
+        
 
-    print(f"{result.get('updatedCells')} cells updated.")
+        time_range = f"'{category}'!K2"
+        current_time = time.strftime("%Y-%m-%d %H:%M:%S")
+        time_body = {
+            'values': [[current_time]]
+        }
+
+        time_result = service.spreadsheets().values().update(
+            spreadsheetId=SPREADSHEET_ID, range=time_range,
+            valueInputOption='RAW', body=time_body).execute()
+
+        print(f"{result.get('updatedCells')} cells updated.")
+
+
 
 
 # The user's credentials are used to login to the Dutchie Backoffice
@@ -204,7 +234,12 @@ login()
 # The fetch_Inventories is repeatedly called to fetch the inventory data from the Dutchie Backoffice (in 30 second intervals) until the program is terminated
 while True:
     products = fetch_Inventories()
-    writeToGoogleSheets(products)
+    print("Data fetched from Dutchie Backoffice @ " + str(time.ctime() + "..."))
+    categorized_products = mapProducts(products)
+    print("Printing products categories...")
+    for category in categorized_products:
+        print(category)
+    writeToGoogleSheets(categorized_products)
     print("Data written to Google Sheets @ " + str(time.ctime()))
     print("Sleeping for 30 seconds...")
     time.sleep(30)
